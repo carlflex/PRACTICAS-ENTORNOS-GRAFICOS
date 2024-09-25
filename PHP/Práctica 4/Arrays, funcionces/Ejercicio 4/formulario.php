e9f4f1efed338e33f2cda0587931f9e012c94e56
NiconiKimg <pedemontenicolas2004@gmail.com>
2024-09-25 20:33:36 -0300

    Practica 4 PHP - Terminada

---
+++ PHP/Práctica 4/Arrays, funcionces/Ejercicio 4/formulario.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Ejercicio 5</title>
+    </head>
+
+    <body>
+        <form action="comprobacion.php" method="post">
+            <input type="text" name="nombre">
+            <input type="submit" value="Comprobar">
+            </form>
+    </body>
+
+</html>
