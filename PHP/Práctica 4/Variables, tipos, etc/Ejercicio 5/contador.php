2d097f634d2e3f4e6056e61973878d53658aa489
NiconiKimg <pedemontenicolas2004@gmail.com>
2024-09-25 13:30:27 -0300

    Práctica 4 PHP. Primera parte. Ej 5

---
+++ PHP/Práctica 4/Variables, tipos, etc/Ejercicio 5/contador.php
@@ -0,0 +1,20 @@
+<?php
+    $archivo = "contador.dat";
+
+    $abrir = fopen($archivo, "r");
+
+    $cont = fread($abrir, filesize($archivo));
+
+    fclose($abrir);
+
+    $abrir = fopen($archivo, "w");
+
+    $cont = $cont + 1;
+
+    $guardar = fwrite($abrir, $cont);
+
+    fclose($abrir);
+
+    echo "<font face='arial' size='3' > Cantidad de visitas:".$cont."</font>"
+    ;
+    ?>
